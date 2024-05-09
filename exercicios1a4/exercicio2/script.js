0e35cf13c3e165b8f22816ba859b2f84397f7662
rafael da silva <126684017+rafiotta@users.noreply.github.com>
2024-05-09 12:08:14 -0300

    projeto criado

---
+++ exercicios1a4/exercicio2/script.js
@@ -0,0 +1,5 @@
+var contador = 10;
+while (contador >= 0) {
+    console.log(contador);
+    contador--;
+}
---
+++ exercicios1a4/exercicio4/script.js
@@ -0,0 +1,7 @@
+var numero = parseInt(prompt("Digite um número para a contagem progressiva:"));
+var contador = 0;
+
+while (contador <= numero) {
+    console.log(contador);
+    contador++;
+}
